fcf95c6e643a87e197baebd89486b5084d87ec5d
Suryapratap Singh <suryprtaps@gmail.com>
2022-07-12 17:20:44 +0530

    fix client id

--- components/MyScripts.tsx
+++ components/MyScripts.tsx
@@ -3,7 +3,7 @@ import Script from 'next/script'
 function MyScripts() {
     return (
         <>
-            <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} crossOrigin="anonymous"/>
+            <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.CLIENT_PUBLIC}`} crossOrigin="anonymous"/>
             <Script
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
                 strategy="lazyOnload"
